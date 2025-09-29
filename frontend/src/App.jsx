4bb238f803fe8eece4ea2494d64f9636a80ca1d1
DKingJosiah <jakpaetuk@gmail.com>
2025-09-29 14:04:37 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import HomePage from "./components/HomePage";
-import RegistrationForm from "./components/RegistrationForm";
+import RegistrationForm from "./components/";
 import AdminLogin from "./components/AdminLogin";
 import AdminDashboard from "./components/AdminDashboard";
 import StatusCheck from "./components/StatusCheck";
